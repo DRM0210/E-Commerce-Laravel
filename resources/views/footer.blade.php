1d9565f9ada4f261b486e96439e3c8fb8a5d2051
DayaRam <87825877+DRM0210@users.noreply.github.com>
2022-11-02 12:38:35 +0530

    Reposit
    First Uploaded Code

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,10 @@
+ <style>
+.bg-4 {
+  background-color: #2f2f2f70;
+  color: #ffffff;
+}
+</style>
+
+<footer class="container-fluid bg-4 text-center">
+  <p>Developed By <a href="#">DRM</a></p>
+</footer> 
